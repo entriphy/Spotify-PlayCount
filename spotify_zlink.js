2e222ded7057b40a856c4262f37ea0930761a2e3
Matthew Finerty <evilarceus@gmail.com>
2020-02-21 01:30:34 -0800

    Use RegEx to capture letter for resolver function
    Prevents #9 from happening with future Spotify updates

--- spotify_zlink.js
+++ spotify_zlink.js
@@ -4,7 +4,7 @@ const path = require("path");
 const readline = require("readline");
 const cfg = require("./config.json");
 const query = '=`hm://album/v1/album-app/album/spotify:album:${e}/desktop?${[`catalogue=${encodeURIComponent(t.catalogue)}`,`locale=${encodeURIComponent(t.locale)}`,`username=${encodeURIComponent(t.username)}`].join("&")}`;' // What to look for in each line to inject code
-const splice = `if(!wSocket){console.log("Connecting to server...");wSocket=new WebSocket("${cfg.useSecure ? "wss" : "ws"}://${cfg.websocket.ipAddress}:${cfg.port}");wSocket.onopen=(event)=>{wSocket.onmessage=(msg)=>{msg=JSON.parse(msg.data);console.log(msg);switch(msg.req){case "playcount":if(msg.id.length!==22)return;const uri=\`hm://album/v1/album-app/album/spotify:album:\${msg.id}/desktop?\${[\`catalogue=\${encodeURIComponent(t.catalogue)}\`, \`locale=\${encodeURIComponent(t.locale)}\`].join("&")}\`;console.log(uri);i.default.resolver.get(uri,(e,t)=>{console.log(e,t);if(e){return wSocket.send(JSON.stringify({uuid:msg.uuid,success:!1,data:e}))}else{return wSocket.send(JSON.stringify({uuid:msg.uuid,success:!0,data:t.getJSONBody()}))}});break;case "hermes":const _uri=msg.uri;console.log(_uri);a.default.resolver.get(_uri,(e,t)=>{console.log(e,t);if(e){return wSocket.send(JSON.stringify({uuid:msg.uuid,success:!1,data:e}))}else{return wSocket.send(JSON.stringify({uuid:msg.uuid,success:!0,data:t.getJSONBody()}))}});break}}}}` // Inject with this string
+const splice = `if(!wSocket){console.log("Connecting to server...");wSocket=new WebSocket("${cfg.useSecure ? "wss" : "ws"}://${cfg.websocket.ipAddress}:${cfg.port}");wSocket.onopen=(event)=>{wSocket.onmessage=(msg)=>{msg=JSON.parse(msg.data);console.log(msg);switch(msg.req){case "playcount":if(msg.id.length!==22)return;const uri=\`hm://album/v1/album-app/album/spotify:album:\${msg.id}/desktop?\${[\`catalogue=\${encodeURIComponent(t.catalogue)}\`, \`locale=\${encodeURIComponent(t.locale)}\`].join("&")}\`;console.log(uri);replaceme.default.resolver.get(uri,(e,t)=>{console.log(e,t);if(e){return wSocket.send(JSON.stringify({uuid:msg.uuid,success:!1,data:e}))}else{return wSocket.send(JSON.stringify({uuid:msg.uuid,success:!0,data:t.getJSONBody()}))}});break;case "hermes":const _uri=msg.uri;console.log(_uri);a.default.resolver.get(_uri,(e,t)=>{console.log(e,t);if(e){return wSocket.send(JSON.stringify({uuid:msg.uuid,success:!1,data:e}))}else{return wSocket.send(JSON.stringify({uuid:msg.uuid,success:!0,data:t.getJSONBody()}))}});break}}}}` // Inject with this string
 const zipAFolder = require("zip-a-folder"); // This is the only zip package that works with Spotify for some reason; AdmZip doesn't work :(
 const process = require("process");
 
@@ -46,8 +46,20 @@ lineReader.on("line", (line) => {
         lines.push(line); // Add line to array
     } else if (line.includes(query)) {
         console.log("[3/7] Attempting to inject code into script...");
+        const uriLetterRegexp = /const (.)=`hm:\/\/album\/v1\/album-app\/album\/spotify:album:\${e}\/desktop/; // Used for the regexp in resolverLetterRegexp
+        const uriLetter = line.match(new RegExp(uriLetterRegexp));
+        if (!uriLetter[1] || uriLetter[1].length !== 1) {
+            console.error("!!! No matches for uriLetter");
+            return;
+        }
+        const resolverLetterRegexp = `(.)\\.default\\.resolver\.get\\(${uriLetter[1]}`; // Finds letter to replace "replaceme" with in the splice variable; prevents https://github.com/evilarceus/Spotify-PlayCount/issues/9. Letter might change with every Spotify update.
+        let resolverLetter = line.match(new RegExp(resolverLetterRegexp));
+        if (!resolverLetter[1] || resolverLetter[1].length !== 1) {
+            console.error("!!! No matches for resolverLetter");
+            return;
+        }
         const idx = line.indexOf(query) + query.length; // Calculation of where to insert the splice variable at
-        line = line.insertAt(idx, splice); // Insert splice at position
+        line = line.insertAt(idx, splice.replace("replaceme", resolverLetter[1])); // Insert splice at position
         lines.push(line);
         injectionSuccessful = true;
     } else {
@@ -56,7 +68,7 @@ lineReader.on("line", (line) => {
 });
 
 lineReader.on("close", () => {
-    if (!injectionSuccessful) console.error("@@@ It appears that the code injection was unsuccessful :( (If possible, submit an issue on GitHub with your zlink_old_***** file that was generated)")
+    if (!injectionSuccessful) console.error("!!! It appears that the code injection was unsuccessful :( (If possible, submit an issue on GitHub with your zlink_old_***** file that was generated)")
     console.log("[4/7] Writing new script to file...");
     fs.writeFileSync("zlink/zlink.bundle.js", lines.join("\n")); // Write to file
     console.log("[5/7] Adding contents of zlink/ to zip...");
